a71ad555463747062d94026e6b780267d972e64f
Ledvin Leiva <ledvin25@hotmail.com>
2025-03-28 11:16:53 -0600

    update

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css'
+
+import Nav from './components/nav/Nav'
+import Hero from './components/hero/Hero'
+
+function App() {
+
+  return (
+    <div>
+      <Nav/>
+      <Hero/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Nav.css'
+
+const Nav = () => {
+    return (
+        <section className="nav">
+            <div className="container">
+                <nav className="nav__container">
+                    <ul className="nav__menu">
+                        <li className="nav__item">
+                            <a href="#" className="nav__link">HOME</a>
+                        </li>
+                        <li className="nav__item">
+                            <a href="#" className="nav__link">ABOUT ME</a>
+                        </li>
+                        <li className="nav__item">
+                            <a href="#" className="nav__link">PROJECTS</a>
+                        </li>
+                        <li className="nav__item">
+                            <a href="#" className="nav__link">CONTACT</a>   
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </section>
+    );
+};
+
+export default Nav;
